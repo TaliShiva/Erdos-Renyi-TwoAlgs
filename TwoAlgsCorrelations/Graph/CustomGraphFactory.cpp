9490ff92b223d20436aad09464e03b7e07789526
gorbunov_nikita_v <gorbunov_nikita_v@mail.ru>
2021-12-16 02:53:53 +0600

    [+] Добавил ещё один тестовый граф

--- TwoAlgsCorrelations/Graph/CustomGraphFactory.cpp
+++ TwoAlgsCorrelations/Graph/CustomGraphFactory.cpp
@@ -22,8 +22,8 @@ IGraphPtr CustomGraphFactory::CreateGraph(const unsigned size)
 	adjacency_matrix[4][1] = true;
 	adjacency_matrix[4][3] = true;*/
 
-	// квадрат и цепочка из двух звеньев
-	adjacency_matrix[0][3] = true;
+	// 6-вершинный: квадрат и цепочка из двух звеньев
+	/*adjacency_matrix[0][3] = true;
 	adjacency_matrix[0][4] = true;
 	adjacency_matrix[1][3] = true;
 	adjacency_matrix[1][4] = true;
@@ -35,17 +35,23 @@ IGraphPtr CustomGraphFactory::CreateGraph(const unsigned size)
 	adjacency_matrix[3][1] = true;
 	adjacency_matrix[4][1] = true;
 	adjacency_matrix[5][1] = true;
-	adjacency_matrix[5][2] = true;
+	adjacency_matrix[5][2] = true;*/
 
-		/*adjacency_matrix[0][2] = true;
-		adjacency_matrix[0][3] = true;
-		adjacency_matrix[1][2] = true;
-		adjacency_matrix[2][3] = true;
+
+	// 4 вершинный алмаз
+	adjacency_matrix[0][1] = true;
+	adjacency_matrix[0][2] = true;
+	adjacency_matrix[0][3] = true;
+
+	adjacency_matrix[1][0] = true;
+	adjacency_matrix[1][2] = true;
 	
-		adjacency_matrix[2][0] = true;
-		adjacency_matrix[3][0] = true;
-		adjacency_matrix[2][1] = true;
-		adjacency_matrix[3][2] = true;*/
+	adjacency_matrix[2][0] = true;
+	adjacency_matrix[2][1] = true;
+	adjacency_matrix[2][3] = true;
+
+	adjacency_matrix[3][0] = true;
+	adjacency_matrix[3][2] = true;
 	
 	return IGraphPtr(new Graph(adjacency_matrix));
 }
--- TwoAlgsCorrelations/main.cpp
+++ TwoAlgsCorrelations/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, // Number of strings in array argv
          char* argv[], // Array of command-line argument strings
          char* envp[]) // Array of environment variable strings
 {
-	const unsigned int graph_size = 6;
+	const unsigned int graph_size = 4;
 	/*const double density = 0.5;
     RandomGraphFactory graph_factory(density);
     auto graphPtr = graph_factory.CreateGraph(graph_size);
